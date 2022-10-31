2e051eca00f9b73765d581f4cbc42ab44138e108
AmgadSa <AmgadSa@outlook.com>
2022-10-31 21:29:37 +0300

    Add UI to the game and refine game mechanics

--- script.js
+++ script.js
@@ -1,50 +1,82 @@
 const choices = ['Rock','Paper','Scissors'];
+const score = [0,0,0];  // [win,loss,round count]
 
 function getComputerChoice() {
-    return choices[Math.round(Math.random()*2)];
+    return choices[Math.floor(Math.random()*3)];
 };
-function play(playerSelection,ComputerSelection){
-    playerSelection = playerSelection[0].toUpperCase().concat(playerSelection.substr(1).toLowerCase());
-    if(!choices.includes(playerSelection)) {return 'invalid choice'};
-    if (playerSelection==ComputerSelection){ return 0}
-    else {
-    switch(playerSelection){
-        case choices[0]:
-            if(ComputerSelection == choices[1]){ return -1};
-        case choices[1]:
-            if(ComputerSelection == choices[2]){ return -1};
-        case choices[2]:
-            if(ComputerSelection == choices[0]){ return -1};
-        default:
-            return 1 ;
-    }}
+function play(player,Computer){
+    if(!choices.includes(player)) {return 'invalid choice'};
+    if (player==Computer){ return 0}
+    switch([player,Computer].toString()){
+        case [choices[0],choices[1]].toString():{
+            return -1;}
+        case [choices[1],choices[2]].toString():{
+            return -1;}
+        case [choices[2],choices[0]].toString():{
+            return -1;}
+        default:{
+            return 1;}
+    }
 }
-function game(){
-    let score = [0,0];
+function startGame(playerSelection,computerSelection){
     let result;
-    let playerSelection;
-    let computerSelection;
-    for(let i=0 ; i<5;i++){
-        playerSelection = prompt('Choose Rock, Paper or Scissors: ') || 'invalid'
-        computerSelection = getComputerChoice();
-        result = play(playerSelection, computerSelection);
-        if(result>0){
-            score[0]++;
-            console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-        } else if (result<0) {
-            score[1]++;
-            console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
-        } else if (result===0){
-            console.log(`Draw! You both picked ${playerSelection}`);
-        } else {
-            console.log(result);
-        }
+    playerSelection = playerSelection || 'invalid';
+    result = play(playerSelection, computerSelection);
+    updateEmoji(computerSelection,playerSelection);
+    score[2]++;
+    if(result>0){
+        score[0]++;
+        updateStatus(`You win! ${playerSelection} beats ${computerSelection}.`);
+    } else if (result<0) {
+        score[1]++;
+        updateStatus(`You lose. ${computerSelection} beats ${playerSelection}.`);
+    } else if (result===0){
+        updateStatus(`Draw! You both picked ${playerSelection}`);
+    } else {
+        updateStatus(result);
+    }
+    if (score[2]<5){
+        titleUpdate(`Round ${score[2]} results:`);
+        return true;
     }
     if (score[0]>score[1]){
-        return `You are the winner. You won ${score[0]} out of 5 rounds`
+        titleUpdate('You are the winner.');
+        updateStatus(`Out of 5 rounds, you have won ${score[0]} rounds
+        and lost ${score[1]} rounds.`);
+        score.splice(0,3,0,0,0)
+        return true;
     } else if (score[0]<score[1]) {
-        return `Try again. You have lost ${score[1]} out of 5 rounds.`
+        titleUpdate('Try Again :(');
+        updateStatus(`Out of 5 rounds, you have lost ${score[1]} rounds
+        and won ${score[0]} rounds`);
+        score.splice(0,3,0,0,0)
+        return true;
     } else {
-        return `It's a draw.`
+        titleUpdate(`It's a draw.`);
+        updateStatus(`Out of 5 rounds, you have won ${score[0]} rounds
+        and lost ${score[1]} rounds.`);
+        score.splice(0,3,0,0,0)
+        return true;
     }
 }
+function updateStatus(news){
+    document.getElementById('status').textContent = news;
+    return true;
+}
+function titleUpdate(title){
+    document.querySelector('.title').textContent = title;
+    return true;
+}
+function updateEmoji(left,right){
+    let emoji = document.querySelector('.emoji');
+    let playerEmoji = document.createElement('button');
+    playerEmoji.className = 'playerEmoji';
+    playerEmoji.textContent = document.querySelector('.'+right).textContent;
+    emoji.textContent = document.querySelector('.'+left).textContent +' | ' 
+    emoji.appendChild(playerEmoji);
+}
+
+for(let button of document.querySelectorAll('.button-container > button')){
+    button.addEventListener('click',(e)=>{
+        startGame(e.target.className,getComputerChoice())
+    })}
